dbf987f4a7d04bf6df63f76f7d3c376059ae126e
heshameldawy <Heshameldawy__@outlook.com>
2023-06-14 23:45:38 -0400

    Demo.js style

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -4,17 +4,6 @@ import React, { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
 const Demo = () => {
-
-  //save in the local storage
-  useEffect( ()=>{
-    const articlesFromLocalStorage = JSON.parse(
-      localStorage.getItem('articles')
-    )
-    if(articlesFromLocalStorage){
-      setAllArticles(articlesFromLocalStorage)
-    }
-  },[]);
-
   const [article,setArticle] = useState({
     url:'',
     summary:'',
@@ -23,6 +12,16 @@ const Demo = () => {
   const [allArticles, setAllArticles] = useState([]);
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  //save in the local storage
+  useEffect( ()=>{
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem('articles')
+    );
+    if(articlesFromLocalStorage){
+      setAllArticles(articlesFromLocalStorage)
+    }
+  },[]);
 
   const handleSubmit = async(e)=>{
     e.preventDefault();
@@ -66,9 +65,30 @@ const Demo = () => {
           </button>
 
         </form>
-         {/* Browser URL Histroy */}
-      </div>
+        {/* Browser URL Histroy */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+            {allArticles.map((item,index) => {
+                <div
+                  key={`link-${index}`}
+                  onClick={()=> setArticle(item)}
+                  className="link_card"
+                >
+                  <div className="copy_btn">
+                      <img src={copy} 
+                            alt="copy icon" 
+                            className="w-[40%] h-[40%] object-contain" 
+                            />
+                  </div>
 
+                  <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                    {item.url}
+                  </p>
+
+                </div>
+            })}
+        </div>
+        
+      </div>
       {/* Display results */}
     </section>
   )
