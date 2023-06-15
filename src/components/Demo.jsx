25f6f5ca9cbec38a43f8e7d2bbd61f000d9588a3
heshameldawy <Heshameldawy__@outlook.com>
2023-06-15 01:11:17 -0400

    project is done

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
-import {  copy, linkIcon, loader, tick } from "../assets";
+import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
+import HistorySearch from "./HistorySearch";
 const Demo = () => {
-  const [article,setArticle] = useState({
-    url:'',
-    summary:'',
+  const [article, setArticle] = useState({
+    url: "",
+    summary: ""
   });
 
   const [allArticles, setAllArticles] = useState([]);
@@ -14,45 +15,54 @@ const Demo = () => {
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
   //save in the local storage
-  useEffect( ()=>{
+  useEffect(() => {
     const articlesFromLocalStorage = JSON.parse(
-      localStorage.getItem('articles')
+      localStorage.getItem("articles")
     );
-    if(articlesFromLocalStorage){
-      setAllArticles(articlesFromLocalStorage)
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
     }
-  },[]);
+  }, []);
 
-  const handleSubmit = async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const {data} = await getSummary({articleUrl:article.url}); /* here where we call the api to make a request to AI api, using Redux */
+    const { data } = await getSummary({
+      articleUrl: article.url
+    }); /* here where we call the api to make a request to AI api, using Redux */
 
-    if(data?.summary){
-      const newArticle = { ...article,summary:data.summary};
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
       const updatedAllArticles = [newArticle, ...allArticles];
       setArticle(newArticle);
       setAllArticles(updatedAllArticles);
-      localStorage.setItem('articles',JSON.stringify(updatedAllArticles))
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
-  }
+  };
 
   return (
     <section className="mt-16 w-full max-w-xl">
       {/* Search */}
       <div className="flex flex-col w-full gap-2">
-        <form action="" className="relative flex justify-center items-center" onSubmit={handleSubmit}>
-          <img 
+        <form
+          action=""
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
             src={linkIcon}
-            alt='link icon'
+            alt="link icon"
             className="absolute left-0 my-2 ml-3 w-5"
           />
-          <input 
+          <input
             type="url"
             placeholder="Enter a URL"
             value={article.url}
-            onChange={(e)=>{setArticle({
-              ... article, url:e.target.value
-            })}}
+            onChange={(e) => {
+              setArticle({
+                ...article,
+                url: e.target.value
+              });
+            }}
             required
             className="url_input peer"
           />
@@ -63,34 +73,41 @@ const Demo = () => {
           >
             <p>↵</p>
           </button>
-
         </form>
         {/* Browser URL Histroy */}
         <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
-          {allArticles.map((item, index) => (
-            <div
-              key={`link-${index}`}
-              onClick={() => setArticle(item)}
-              className="link_card"
-            >
-              <div className="copy_btn">
-                <img
-                  src={copy}
-                  alt="copy icon"
-                  className="w-[40%] h-[40%] object-contain"
-                />
-              </div>
-
-              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
-                {item.url}
-              </p>
-            </div>
-          ))}
+          <HistorySearch allArticles={allArticles} setArticle={setArticle} />
         </div>
       </div>
       {/* Display results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            An error occured
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
